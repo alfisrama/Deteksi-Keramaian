97a5c3f79110fc3e1992e6e08b4d075e94131a64
alfisrama <39265253+alfisrama@users.noreply.github.com>
2020-07-02 22:08:14 +0700

    Update register.php

--- register.php
+++ register.php
@@ -57,10 +57,19 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>
-        $('#confirm_password').on('change', function () {
+        $('#confirm_password').on('keyup', function () {
             if ($('#password').val() == $('#confirm_password').val()) {
                 $('#messageConfirm').html('Match').css('color', 'green');
-                document.getElementById("submit").disabled = false;
+		            document.getElementById("submit").disabled = false;
+            } else {
+                $('#messageConfirm').html('Not Match').css('color', 'red');
+                document.getElementById("submit").disabled = true;
+            }
+        });
+	    $('#password').on('keyup', function () {
+          if ($('#password').val() == $('#confirm_password').val()) {
+              $('#messageConfirm').html('Match').css('color', 'green');
+		          document.getElementById("submit").disabled = false;
             } else {
                 $('#messageConfirm').html('Not Match').css('color', 'red');
                 document.getElementById("submit").disabled = true;
